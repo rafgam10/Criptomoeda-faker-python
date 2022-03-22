19d898a3d7ca11dfdb8f0d78a8eaf55e82f2d038
Rafael Timóteo <rafgam19@gmail.com>
2022-03-21 22:41:52 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Criptomoeda Faker em Python
+
+def main():
+    while 1:
+        print()
+        t = int(input("Digite o tempo de trabalho em s: "))
+        q = t/2
+        print("Você minerrou: ",q, "ALC")
+        print("----------------------------------------")
+main()
